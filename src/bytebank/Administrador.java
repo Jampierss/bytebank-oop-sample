eaa849249783e3d4664660061e0eceeb25725cec
Jeampierss <jimmyflorian2005@gmail.com>
2023-07-01 18:49:19 -0500

    Implementing Autenticable interface to Administrador class

--- src/bytebank/Administrador.java
+++ src/bytebank/Administrador.java
@@ -1,6 +1,8 @@
 package bytebank;
 
-public class Administrador extends Funcionario{
+import controller.Autenticable;
+
+public class Administrador extends Funcionario implements Autenticable {
 
     private String clave;
 
@@ -10,5 +12,15 @@ public class Administrador extends Funcionario{
     @Override
     public double getBonificacion() {
         return this.getSalario();
+    }
+
+    @Override
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    @Override
+    public boolean iniciarSesion(String clave) {
+        return this.clave.equals(clave);
     }
 }
