23d52d7e705095ffca56facf528a9bd72c1150b2
Jeampierss <jimmyflorian2005@gmail.com>
2023-06-30 19:26:32 -0500

    Adding a controller of bonification

---
+++ src/controller/ControlBonificacion.java
@@ -0,0 +1,14 @@
+package controller;
+
+import bytebank.Funcionario;
+
+public class ControlBonificacion {
+
+    private double suma;
+
+    public double registrarSalario(Funcionario funcionario) {
+        this.suma = funcionario.getBonificacion() + this.suma;
+        System.out.println("Calculo actual: " + this.suma);
+        return this.suma;
+    }
+}
