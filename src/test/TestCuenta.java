a2e2599930c6ae64a1b728730d4204f052040da3
Jeampierss <jimmyflorian2005@gmail.com>
2023-06-30 20:25:22 -0500

    Testing Cuenta model

--- src/test/TestCuenta.java
+++ src/test/TestCuenta.java
@@ -1,2 +1,21 @@
-package test;public class TestCuenta {
+package test;
+
+import models.Cuenta;
+import models.CuentaAhorro;
+import models.CuentaCorriente;
+
+public class TestCuenta {
+    public static void main(String[] args) {
+        CuentaCorriente cc = new CuentaCorriente(1, 1);
+        CuentaAhorro ca = new CuentaAhorro(2, 3);
+
+        cc.deposita(2000);
+        cc.transfiere(1000, ca);
+
+        System.out.println(cc.getSaldo());
+        System.out.println(ca.getSaldo());
+
+        System.out.println(Cuenta.getTotal());
+    }
+
 }
