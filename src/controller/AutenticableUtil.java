06dc4952617a05a221ce889397a6adaac52bedcf
Jeampierss <jimmyflorian2005@gmail.com>
2023-07-01 19:51:12 -0500

    Update AutenticableUtil class

--- src/controller/AutenticableUtil.java
+++ src/controller/AutenticableUtil.java
@@ -1,2 +1,14 @@
-package controller;public class AutenticableUtil {
+package controller;
+public class AutenticableUtil {
+
+    private String clave;
+
+    public boolean iniciarSesion(String clave) {
+        return this.clave.equals(clave);
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
 }
